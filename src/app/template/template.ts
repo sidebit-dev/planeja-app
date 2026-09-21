40b030d09ac869c7015a8fe55f1777cc7207372c
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-21 20:08:31 -0300

    Create template component: npx ng generate component template --skip-tests

---
+++ src/app/template/template.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  imports: [],
+  templateUrl: './template.html',
+  styleUrl: './template.scss',
+})
+export class Template {}
