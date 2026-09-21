87421b7cb353e3cf940f09e214f12ccc95744374
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-21 20:34:29 -0300

    Developing the application template

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Template } from './template/template';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Template],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('planeja-app');
