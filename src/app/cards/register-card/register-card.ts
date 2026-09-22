b89e2c1e752cde7a89503418db3d41cd975348a0
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-21 23:28:19 -0300

    Reactive Forms for capturing form data

--- src/app/cards/register-card/register-card.ts
+++ src/app/cards/register-card/register-card.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
+interface RegisterCardForm {
+  name: FormControl<string>;
+  flag: FormControl<string>;
+}
 
 @Component({
   selector: 'app-register-card',
-  imports: [],
+  imports: [ReactiveFormsModule], // para ligar o HTML com os campos definidos
   templateUrl: './register-card.html',
   styleUrl: './register-card.scss',
 })
-export class RegisterCard {}
+export class RegisterCard implements OnInit {
+  // Este ! não é um campo opcional
+  form!: FormGroup<RegisterCardForm>;
+  // Qdo acessado a rota desta página, vai ser executado
+  ngOnInit(): void {
+    this.form = new FormGroup<RegisterCardForm>({
+      name: new FormControl('', { nonNullable: true, validators: Validators.required }),
+      flag: new FormControl('', { nonNullable: true, validators: Validators.required }),
+    });
+  }
+
+  handleSubmit() {
+    console.log(this.form.value);
+  }
+}
